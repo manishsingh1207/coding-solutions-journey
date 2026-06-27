db3bc4aff93cde8d4c9eb53f8fb0afb7e2745904
Manish Singh <79357395+manishsingh1207@users.noreply.github.com>
2026-06-27 19:59:59 +0530

    Time: 1 ms (87.15%), Space: 53.8 MB (90.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+   var map = {};
+   for(let i=0;i<nums.length;i++)
+   {
+        const targetvalue =target-nums[i];
+
+        if(targetvalue in map)
+        {
+            return [map[targetvalue],i];
+        }
+
+        map[nums[i]]=i;
+   } 
+   return null;
 };
